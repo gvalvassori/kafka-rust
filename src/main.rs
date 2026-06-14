0f067255efab5d40d6279e5b30e4daf867fc0cca
Gaston Valvassori <gtn.valvassori@gmail.com>
2026-06-14 16:35:41 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
-                handle_stream(&stream);
+                handle_stream(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -26,20 +26,39 @@ fn main() {
     }
 }
 
-fn handle_stream(mut stream: &TcpStream) {
-    println!("Handling stream");
-    let mut buffer = [0u8; 64];
-    let _ = stream.read(&mut buffer);
-    let message_size_bytes: [u8; 4] = buffer[0..4].try_into().unwrap();
-    // Convert big-endian bytes to 32-bits integer
-    // let message_size = i32::from_be_bytes(message_size_bytes);
-    let correlation_id_bytes: [u8; 4] = buffer[8..12].try_into().unwrap();
+fn handle_stream(stream: &mut TcpStream) {
+    println!("handling stream");
+
+    while let Some(body) = read_one_request(stream) {
+        let response = build_response(body);
+        let _ = stream.write_all(&response);
+    }
+}
+
+fn read_one_request(stream: &mut TcpStream) -> Option<Vec<u8>> {
+    // Read the first 4 bytes of a stream which is the message_size
+    // and then read the following message_size bytes that is the body.
+    let mut body_size = [0u8; 4];
+    if stream.read_exact(&mut body_size).is_err() {
+        return None;
+    }
+    let body_len = u32::from_be_bytes(body_size) as usize;
+
+    let mut payload = vec![0u8; body_len];
+    stream.read_exact(&mut payload).ok()?;
+    Some(payload)
+}
+
+fn build_response(payload: Vec<u8>) -> Vec<u8> {
+    println!("building response");
+    let correlation_id_bytes: [u8; 4] = payload[8..12].try_into().unwrap();
     // let correlation_id = i32::from_be_bytes(correlation_id_bytes);
-    let api_key_bytes: [u8; 2] = buffer[4..6].try_into().unwrap();
+    let api_key_bytes: [u8; 2] = payload[4..6].try_into().unwrap();
+    // Convert big-endian bytes to 32-bits integer
     let api_key = i16::from_be_bytes(api_key_bytes);
     let api_keys_arr = api_versions_response(api_key);
     let api_keys_byte = serialize_api_keys(&api_keys_arr);
-    let api_version_bytes: [u8; 2] = buffer[6..8].try_into().unwrap();
+    let api_version_bytes: [u8; 2] = payload[6..8].try_into().unwrap();
     let api_version = i16::from_be_bytes(api_version_bytes);
     let error_code: [u8; 2] = check_valid_api_version(api_version);
 
@@ -59,7 +78,7 @@ fn handle_stream(mut stream: &TcpStream) {
     let mut response: Vec<u8> = Vec::new();
     response.extend_from_slice(&message_size.to_be_bytes());
     response.extend_from_slice(&body);
-    let _ = stream.write(&response);
+    response
 }
 
 fn check_valid_api_version(version: i16) -> [u8; 2] {
